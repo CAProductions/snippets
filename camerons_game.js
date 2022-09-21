5df9fdacf1121d6ee2db8025c38185523e17a0fb
CAProductions <CAProductions2021@outlook.com>
2022-09-21 11:15:43 -0400

    hi

--- camerons_game.js
+++ camerons_game.js
@@ -1,5 +1,26 @@
+// Intro
 alert("A CAM0studios production");
 alert("Together with drsmashsenstien");
 alert("We present:");
 alert("[name yet to be decided]");
 confirm("Start game?");
+
+// Story Start
+alert("You wake up in a dark room.");
+if (!yn("You see a mysterious device on the floor. Pick it up?")) {
+    alert ("The device blew up when you decided not to pick, and now you are dead. Goodbye!");
+}
+
+
+
+
+
+
+
+
+//--------Functions-----------------------------------------------------------------------------------------
+function yn(text) {
+    let answer = prompt(`${text} (y/n)`)
+    answer = answer.trim();
+    return answer == "y" || answer == "Y";
+}
