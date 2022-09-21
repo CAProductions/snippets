39913fef0fa5cb3c1858c67e511882529f64b1ca
CAProductions <CAProductions2021@outlook.com>
2022-09-21 14:56:57 -0400

    update

--- camerons_game.js
+++ camerons_game.js
@@ -3,18 +3,21 @@ var p = {};
 p.health = 12;
 p.inventory = [];
 p.weapon = 0;
+p.armor = 0;
+var weapons = [weaponData("the explosion", 120), weaponData("a tree branch", 1)]
+var armor = [armorData("", 0), armorData("some old clothes", 1)];
 
 // Intro
 alert("A CAM0studios production");
-alert("Together with drsmashsenstien");
+alert("Together with DrSmashsenstien");
 alert("We present:");
-alert("[name yet to be decided]");
+alert("[name tbd]");
 confirm("Start game?");
 
-// Story Start
+// Story Intro
 alert("You wake up in a dark room.");
 if (!yn("You see a mysterious device on the floor. Pick it up?")) {
-    alert ("The device blew up when you decided not to pick, and now you are dead. Goodbye!");
+    dmg(120, "", "You don't pick it up. You're starting to feel hungry. You go back to sleep, never to wake up /n /n ---GAME OVER---");
 }
 
 
@@ -30,11 +33,24 @@ function yn(text) {
     answer = answer.trim();
     return answer == "y" || answer == "Y";
 }
-function dmg(amt, enemy) {
+function dmg(amt, enemy, message) {
     p.health -= amt;
-    if (p.health > 0) {
-        alert(`You took ${amt} damage from ${enemy.name} with ${enemy.weapon}. You now have ${p.health} health.`);
-    } else {
-        alert(`You took ${amt} damage from ${enemy.name} with ${enemy.weapon}. You are now dead`);
-    };
+    if(message){
+        alert(message);
+    } else{
+        if (p.health > 0) {
+            alert(`You took ${amt} damage from ${enemy.name} with ${enemy.weapon}. You now have ${p.health} health.`);
+        } else {
+            alert(`You took ${amt} damage from ${enemy.name} with ${enemy.weapon}. You are now dead.`);
+        };
+    }
+}
+function enemydata(name, weaponInd, armorInd) {
+    return {"name": name, "weapon": weapons[weaponInd], "armor": armor[armorInd]}
+}
+function weaponData(name, atk) {
+    return {"name": name, "atk": atk}
+}
+function armorData(name, def) {
+    return {"name": name, "def": def}
 }
